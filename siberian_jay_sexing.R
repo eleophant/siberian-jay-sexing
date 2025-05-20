a138af03d007f4badac5fcddfe36a4ab50cf34a8
eleophant <eleonore.e.lt@gmail.com>
2025-05-20 12:27:24 -0600

    add initial files

---
+++ siberian_jay_sexing.R
@@ -0,0 +1,22 @@
+#####################
+#
+# siberian jay sexing
+#
+#####################
+
+# packages ----
+library(tidyverse)
+
+# data input ----
+sexing = read_csv("siberian_jay_sexing.csv")
+metadata_1 = read_csv("df_metadata_sib.csv")
+metadata_2 = read_csv("fasteris_samples_metadata.csv")
+
+# sexing ----
+# get ring numbers
+sexing = 
+
+# assign sex (if allele1 = allele2, then male)
+
+# metadata ----
+# get all ring numbers from metadata 1 and 2
